c3039e71cecc712764d973fb63abdc203aa0801b
schottju <schott.julien@wanadoo.fr>
2015-12-17 07:58:10 +0100

    corrections hospit

--- app/models/hospitacte.rb
+++ app/models/hospitacte.rb
@@ -1,6 +1,7 @@
 class Hospitacte < ActiveRecord::Base
   has_and_belongs_to_many :hospits
-  belongs_to :veterinarians  
+  belongs_to :veterinarian
+  has_and_belongs_to_many :veterinarians  
 #  belongs_to :medoc
 
 #  accepts_nested_attributes_for :medocs
